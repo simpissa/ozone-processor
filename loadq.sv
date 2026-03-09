deeaf971491675d2637374c815932d7bf4150d66
wcm4284 <willcmatherne@gmail.com>
2026-03-09 12:14:23 -0500

    Update loadq.sv
    Should be complete besides 2 possible errors:
    
    1. relative age is wrong for traces
    2. might need to make a ready/valid protocol for reading completes to the system

--- loadq.sv
+++ loadq.sv
@@ -17,11 +17,12 @@ module load_queue #(
     output logic         sq_query_valid,
     output logic [47:0]  sq_query_addr,
     output logic [3:0]   sq_query_id,
+    output logic [$clog2(LQ_SIZE-1:0] sq_query_tail,
     input  logic         sq_forward_valid,
     input  logic [63:0]  sq_forward_data,
     input  logic         sq_conflict,
-
-    // need a way for sq to tell us that they didn't find it so we can query cache
+    input  logic         sq_miss, // use this so the sq can tell us they didn't find anything
+    input  logic [$clog2(LQ_SIZE)-1:0] sq_curr_tail; // current tail
 
     // Request to L1 data cache
     output logic         l1_req_valid,
@@ -40,13 +41,18 @@ module load_queue #(
     output logic [63:0]  load_complete_data
 );
 
+localparam IDX_W = $clog2(LQ_SIZE);
+localparam INVALID_IDX = LQ_SIZE;
+
 // TODO: add "ROB idx" (count # of traces) to store relative age
 typedef struct packed {
     logic valid;
     logic [ID_W-1:0] id;
+    logic [IDX_W-1:0] sq_tail;
     logic [47:0] vaddr;
     logic addr_valid;
     logic issued;
+    logic conflict;
     logic completed;
     logic [63:0] data;
 } lq_entry;
@@ -58,13 +64,11 @@ typedef enum logic [2:0] {
     OP_TLB_FILL    = 4  // shouldn't need (unless we drive the tlb?)
 } op_code;
 
-localparam IDX_W = $clog2(LQ_SIZE);
-
 logic [IDX_W-1:0] head;
 logic [IDX_W-1:0] tail;
 
 logic [LQ_SIZE-1:0] ready;
-logic [ID_W-1:0] id_map [0:LQ_SIZE-1];
+logic [ID_W:0] id_map [0:LQ_SIZE-1];
 lq_entry queue [0:LQ_SIZE-1];
 
 // stages
@@ -80,68 +84,91 @@ logic [IDX_W-1:0] issue_idx;
 /* initialize everything important to 0 */
 initial begin
     for (int i = 0; i < LQ_SIZE; ++i) begin
-        queue[i].valid = 0;
-        id_map[i]      = 0;
+        queue[i].valid     = 0;
+        queue[i].issued    = 0;
+        queue[i].conflict  = 0;
+        queue[i].completed = 0;
+        id_map[i]          = INVALID_IDX;
     end
 
     head = 0;
     tail = 0;
     issue_storeq = 0;
     issue_cache = 0;
+    waiting_issue = 1;
 
 end
-
 
 always_ff @(posedge clk) begin
     
     // set our queue and ptrs back to 0
     if (reset) begin
 
-    for (int i = 0; i < LQ_SIZE; ++i) begin
-        queue[i].valid      <= 1'b0;
-        queue[i].vaddr      <= 48'b0;
-        queue[i].addr_valid <= 1'b0;
-        queue[i].issued     <= 1'b0;
-        queue[i].completed  <= 1'b0;
-        queue[i].data       <= 64'b0;
-        id_map[i]           <= 0;
-    end
+        for (int i = 0; i < LQ_SIZE; ++i) begin
+            queue[i].valid      <= 1'b0;
+            queue[i].sq_tail    <= 0;
+            queue[i].vaddr      <= 48'b0;
+            queue[i].addr_valid <= 1'b0;
+            queue[i].issued     <= 1'b0;
+            queue[i].conflict   <= 1'b0;
+            queue[i].completed  <= 1'b0;
+            queue[i].data       <= 64'b0;
+            id_map[i]           <= INVALID_IDX;
+        end
 
-    head <= 0;
-    tail <= 0;
-    issue_storeq <= 0;
-    issue_cache  <= 0;
+        head <= 0;
+        tail <= 0;
+        issue_storeq  <= 0;
+        issue_cache   <= 0;
+        waiting_issue <= 1;
 
     end
     
     // new trace coming in! pick it up if necessary
     if (trace_valid) begin
 
+        trace_collected <= 0;
+
         if (trace_op == OP_MEM_LOAD) begin
-            assert(!queue[tail].valid);
+            
+            // check that the queue isn't full
+            // how do i tell that it's been collected? 
+            if (tail != head) begin
+                assert(!queue[tail].valid);
 
-            // this will need to be changed to make sure we have room to accept
-            // will probably have to figure out a way to tell the trace issuer that we haven't collected
-            // for now, whatever
-            assert(tail != head); 
+                trace_collected <= 1;
 
-            id_map[trace_id] <= tail;
+                id_map[trace_id] <= tail;
 
-            queue[tail].id          <= trace_id;
-            queue[tail].vaddr       <= trace_vaddr;
-            queue[tail].addr_valid  <= trace_vaddr_is_valid;
+                queue[tail].id          <= trace_id;
+                queue[tail].vaddr       <= trace_vaddr;
+                queue[tail].addr_valid  <= trace_vaddr_is_valid;
+                queue[tail].sq_tail     <= sq_curr_tail;
+                queue[tail].issued      <= 0;
+                queue[tail].conflict    <= 0;
+                queue[tail].completed   <= 0;
 
-            tail <= tail + 1;
+                tail <= tail + 1;
+            end
 
         end else if (trace_op == OP_MEM_RESOLVE) begin
-            assert(queue[id_map[trace_id]].valid);
             assert(trace_vaddr_is_valid);
 
-            queue[id_map[trace_id]].vaddr       <= trace_vaddr;
-            queue[id_map[trace_id]].addr_valid  <= trace_vaddr_is_valid;
+            trace_collected <= 1;
+            
+            // do we actually have something for this trace_id, or is it just for a store?
+            if (id_map[trace_id] != INVALID_IDX) begin
+                assert(queue[id_map[trace_id]].valid);
 
+                queue[id_map[trace_id]].vaddr       <= trace_vaddr;
+                queue[id_map[trace_id]].addr_valid  <= trace_vaddr_is_valid;
+            end
+
+            // reset all conflict bits
+            for (int i = 0; i < LQ_SIZE; ++i) begin
+                queue[i].conflict <= 0;
+            end
         end
-
     end
 
     if (waiting_issue) begin
@@ -157,35 +184,43 @@ always_ff @(posedge clk) begin
 
             sq_query_addr  <= queue[next_issue_idx].vaddr;
             sq_query_id    <= queue[next_issue_idx].id;
+            sq_query_tail  <= queue[next_issue_idx].sq_tail;
             sq_query_valid <= 1;
             
             // its in the issue pipeline now
             issue_idx <= next_issue_idx; // next is free to change now, which can most certainly happen while we're in issue
             queue[next_issue_idx].issued <= 1;
             waiting_issue <= 0;
-
         end
-
     end
 
 
     if (issue_storeq) begin
         // we issued to the storeq, waiting for a response
 
-        // TODO: need to create an option for sq to tell us it wasn't there
-        // so we can query the cache
-        if (sq_forward_valid) begin
+        if (sq_miss) begin
+            // no data to forward, so query the cache
+            if (l1_req_ready) begin
+                l1_req_vaddr <= queue[issue_idx].vaddr; 
+                l1_req_id    <= queue[issue_idx].id;
+                l1_req_valid <= 1;
+                issue_cache  <= 1;
+                issue_storeq <= 0;
+            end
+
+        end else if (sq_forward_valid) begin
             queue[issue_idx].completed <= 1;
             queue[issue_idx].data <= sq_forward_data;
             issue_storeq <= 0;
             waiting_issue <= 1;
         end else if (sq_conflict) begin
-            // TODO
+            queue[issue_idx].conflict <= 1;
+            queue[issue_idx].issued <= 0;
+            issue_storeq <= 0;
+            waiting_issue <= 1;
         end
 
     end
-
-    // TODO: create a stage where we wait to make sure the cache is free for a request
 
     if (issue_cache) begin
         // we issued to the cache, waiting for a response
@@ -197,6 +232,7 @@ always_ff @(posedge clk) begin
 
             issue_cache <= 0;
             waiting_issue <= 1;
+            l1_req_valid <= 0;
         end
     end
     
@@ -208,6 +244,7 @@ always_ff @(posedge clk) begin
         load_complete_valid <= 1;
 
         // TODO: do we need to wait for data to be read? or can we just move on
+        // also: this is a bug if LQ_SIZE is not a power of 2
         head <= head + 1;
     end else begin
         load_complete_id <= 0;
@@ -221,7 +258,8 @@ end
 // maintain ready list and the idx of which entry we should issue next when possible
 always_comb begin
     for (int i = 0; i < LQ_SIZE; ++i) begin
-        ready[i] = queue[i].valid && queue[i].addr_valid && !queue[i].issued;
+        ready[i] = queue[i].valid && queue[i].addr_valid 
+                && !queue[i].issued && !queue[i].conflict;
     end
 
     found_issue = 0;
