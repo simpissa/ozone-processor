1c76ebcb50491da50915e9a39c8fd84c562d21f5
simpissa <matthewzhao420@gmail.com>
2026-04-08 17:50:04 -0500

    add control transfer instructions

--- frontend/decoder.sv
+++ frontend/decoder.sv
@@ -6,12 +6,15 @@ typedef enum logic [2:0] {
     FU_LOGIC,
     FU_AGU,
     FU_FPU,
-    FU_MEM
+    FU_MEM,
+    FU_NONE
 } fu_t; // functional units
 
 typedef enum logic [4:0] {
     OP_ADD,
-    OP_SUB, // TODO: add more
+    OP_SUB,
+    OP_COND_CHECK,
+    OP_NOP
 } fu_op_t; // which op to execute, depends on our fus
 
 typedef enum logic [5:0] {
@@ -19,6 +22,7 @@ typedef enum logic [5:0] {
     // TODO: add rest of instructions
     I_B, I_BCOND, I_BL, I_RET,
     I_NOP, I_ERET, I_MRS, I_MSR, I_SVC,
+    I_UNKNOWN
 } instr_id_t;
 
 module decoder (
@@ -48,15 +52,16 @@ module decoder (
     output logic        rs2_valid,
     output logic [63:0] imm, // immediate 
     output logic        imm_valid,
+    output logic        src1_is_pc, // use pc as source register (for branching ops)
 
     // Multi-uop control
     output logic        first_uop, // allocate rob entry on this
     output logic        last_uop, 
-    output logic        is_sequential, // data dependence between uops
+    output logic        is_sequential, // does the current uop depend on a previous uop
 
     // Extra data
+    output logic        is_branch,
     output logic [3:0]  cond,
-    output logic [63:0] branch_target,
     output logic [3:0]  spr_id // special purpose register id
     // TODO: add more if uop needs it
 );
@@ -77,15 +82,66 @@ module decoder (
     
     instr_id_t instr_id;
 
+    // fields
+    logic [4:0] Rn_field;
+
+    logic [25:0] simm26; // b1
+    logic [18:0] simm19; // i2, b2
+
+    logic [3:0]  cond_field; // b2
+
+    logic [63:0] bcond_offset;
+    logic [63:0] b_offset;
+
+    assign Rn_field   = instr[9:5];
+    assign simm26     = instr[25:0];
+    assign simm19     = instr[23:5];
+    
+    assign cond_field = instr[3:0];
+    // from ARM manual: bits(64) offset = SignExtend(imm19:'00', 64);
+    assign bcond_offset = {{43{simm19[18]}}, simm19, 2'b00};
+    assign b_offset     = {{36{simm26[25]}}, simm26, 2'b00};
+
     // Identify opcode
     always_comb begin
-        instr_id = 
+        instr_id = I_UNKNOWN;
+
+        unique casez (instr)
+        
+            // Control transfer
+            32'b000101??????????????????????????: instr_id = I_B;
+            32'b01010100???????????????????0????: instr_id = I_BCOND;
+            32'b100101??????????????????????????: instr_id = I_BL;
+            32'b1101011001011111000000?????00000: instr_id = I_RET;
+
+            default: instr_id = I_UNKNOWN;
+        endcase
     end
     
     // set outputs
     always_comb begin
-        case instr_id:
+        // set defaults
+        valid_out      = valid_in;
+        fu_select      = FU_NONE;
+        fu_op          = OP_NOP;
+        rd             = 5'd31;
+        r_dest_valid   = 1'b0;
+        rs1            = 5'd31;
+        rs1_valid      = 1'b0;
+        rs2            = 5'd31;
+        rs2_valid      = 1'b0;
+        imm            = 64'd0;
+        imm_valid      = 1'b0;
+        src1_is_pc     = 1'b0;
+        first_uop      = (uop_counter == 0);
+        last_uop       = 1'b1; // default to 1, set to 0 if not
+        is_sequential  = 1'b0;
+        is_branch      = 1'b0;
+        cond           = 4'd0;
+        spr_id         = 4'd0;
 
+
+        case (instr_id)
             // =============================================================
             // DATA TRANSFER
             // =============================================================
@@ -103,18 +159,70 @@ module decoder (
             // CONTROL TRANSFER
             // =============================================================
             I_B: begin
-            
+                is_branch     = 1'b1;
+                fu_select     = FU_ALU;
+                fu_op         = OP_ADD;
+                imm           = b_offset;
+                imm_valid     = 1'b1;
+                src1_is_pc    = 1'b1;
             end
 
             I_BCOND: begin
+                is_branch     = 1'b1;
+                cond          = cond_field;
+
                 case (uop_counter)
                     0: begin // COND_CHECK
+                        fu_select = ; // TODO: which fu does COND_CHECK go to?
+                        fu_op     = OP_COND_CHECK;
+                        last_uop  = 1'b0;
                     end
 
                     1: begin // ADD
+                        fu_select     = FU_ALU;
+                        fu_op         = OP_ADD;
+                        imm           = bcond_offset;
+                        imm_valid     = 1'b1;
+                        src1_is_pc    = 1'b1;
+                        is_sequential = 1'b0;
                     end
                 endcase
             end
+
+            I_BL: begin
+                // ARM page 60: Branch with Link branches to a PC-relative offset, setting the register X30 to PC+4
+                is_branch     = 1'b1;
+                case (uop_counter)
+                    0: begin
+                        fu_select    = FU_ALU;
+                        fu_op        = OP_ADD;
+                        imm          = 64'd4;
+                        imm_valid    = 1'b1;
+                        src1_is_pc   = 1'b1;
+                        rd           = 5'd30;
+                        r_dest_valid = 1'b1;
+                        last_uop     = 1'b0;
+                    end
+
+                    1: begin
+                        fu_select     = FU_ALU;
+                        fu_op         = OP_ADD;
+                        imm           = b_offset;
+                        imm_valid     = 1'b1;
+                        src1_is_pc    = 1'b1;
+                        is_sequential = 1'b0;
+                    end
+                endcase
+            end
+
+            I_RET: begin
+                is_branch = 1'b1;
+                fu_select = FU_LOGIC;
+                fu_op     = ; // TODO: which op is OR w/ XZR?
+                rs1       = Rn_field;
+                rs1_valid = 1'b1;
+            end
+
             // =============================================================
             // MISC
             // =============================================================
@@ -122,7 +230,9 @@ module decoder (
             // =============================================================
             // FLOATING-POINT
             // =============================================================
-            
+
+            default: begin
+            end
         endcase
     end
 
