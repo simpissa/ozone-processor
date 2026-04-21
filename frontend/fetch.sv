f9dfcffafbb174da1ae7a86ebd58be9ea0442df2
wcm4284 <willcmatherne@gmail.com>
2026-04-21 18:45:24 -0500

    fetch (hopefully) complete

--- frontend/fetch.sv
+++ frontend/fetch.sv
@@ -6,7 +6,11 @@ module fetch (
     input logic clk,
     input logic reset,
     input logic flush, // not sure we need this, could just use execute
-
+    
+    // or is it this we don't need?
+    // should be pretty easy to just get the flush signal and say goodbye
+    // if that's the case, we still need to know where our pc should be, so
+    // we need to keep one input for that
     // execute/commit
     input logic         exe_valid_i,
     input logic         exe_branch_i,
@@ -45,11 +49,12 @@ module fetch (
 );
 
     logic [63:0] pc;
+    logic [30:0] paddr_offset; // used to determine if we need to ask mem for another read or we can just query;
 
-
+    // Note: This stage information is mostly used for debugging. it does little
+    // in way of actually moving information between stages
     typedef struct packed { 
         logic        valid;
-        logic        compl;
         logic [63:0] vaddr;
         logic [29:0] paddr;
         logic [31:0] instr;
@@ -74,28 +79,39 @@ module fetch (
     stagei_t stage1;
     stageii_t stage2;
     stageiii_t stage3;
-
-
-    logic stage1_stall, stage2_stall, stage3_stall;
+    stageiii_t stage4;
+    logic stage1_stall, stage2_stall, stage3_stall, stage4_stall;
 
     task print_stages;
         if (DBG) begin
-            $display("\nStage -> | %-16s | %-16s | %-16s |", "Stage I", "Stage II", "Stage III");
-            $display("Valid -> | %d%15s | %d%15s | %d%15s |", stage1.valid, "", stage2.valid, "", stage3.valid, "");
-            $display("Stall -> | %d%15s | %d%15s | %d%15s |", stage1_stall, "", stage2_stall, "", stage3_stall, "");
-            $display("Vaddr -> | %016x | %016x | %016x |", stage1.vaddr, stage2.vaddr, stage3.vaddr);
-            $display("Paddr -> | %16s | %08x%8s | %08x%8s |", "N/A", stage2.paddr, "", stage3.paddr, "");
-            $display("Instr -> | %16s | %16s | %08x%8s |\n", "N/A", "N/A", stage3.instr, "");
+            $display("\nStage -> | %-16s | %-16s | %-16s | %-16s |", "Stage I", "Stage II", "Stage III", "Stage IV");
+            $display("Valid -> | %d%15s | %d%15s | %d%15s | %d%15s |", stage1.valid, "", stage2.valid, "", stage3.valid, "", stage4.valid, "");
+            $display("Stall -> | %d%15s | %d%15s | %d%15s | %d%15s |", stage1_stall, "", stage2_stall, "", stage3_stall, "", stage4_stall, "");
+            $display("Vaddr -> | %016x | %016x | %016x | %016x |", stage1.vaddr, stage2.vaddr, stage3.vaddr, stage4.vaddr);
+            $display("Paddr -> | %16s | %08x%8s | %08x%8s | %08x%8s |", "N/A", stage2.paddr, "", stage3.paddr, "", stage4.paddr, "");
+            $display("Instr -> | %16s | %16s | %08x%8s | %08x%8s |\n", "N/A", "N/A", stage3.instr, "", stage4.instr, "");
         end
 
     endtask
 
     task move_stages;
+
+        if (~stage4_stall) begin
+            stage4.valid <= 0;
+            stage4.vaddr <= '0;
+            stage4.paddr <= '0;
+            stage4.instr <= '0;
+        end
         
         if (~stage3_stall) begin
+            stage4.valid <= stage3.valid;
+            stage4.vaddr <= stage3.vaddr;
+            stage4.paddr <= stage3.paddr;
+            assert(paddr_offset[8:0] <= 480);
+            stage4.instr <= imem_rdata_i[paddr_offset[8:0] +: 32];
+
             // move stage 3 out
             stage3.valid <= 0;
-            stage3.compl <= 0;
             stage3.vaddr <= '0;
             stage3.paddr <= '0;
             stage3.instr <= '0;
@@ -105,7 +121,7 @@ module fetch (
         if (~stage2_stall) begin
             stage3.valid <= stage2.valid;
             stage3.vaddr <= stage2.vaddr;
-            stage3.paddr <= stage2.paddr;
+            stage3.paddr <= itlb_paddr_i;
 
             stage2.valid <= 0;
             stage2.vaddr <= '0;
@@ -116,21 +132,45 @@ module fetch (
         if (~stage1_stall) begin
             stage2.valid <= stage1.valid;
             stage2.vaddr <= stage1.vaddr;
-            
         end
 
+        if (flush) begin
+            stage4.valid <= 0;
+            stage4.vaddr <= '0;
+            stage4.paddr <= '0;
+            stage4.instr <= '0;
+
+            stage3.valid <= 0;
+            stage3.vaddr <= '0;
+            stage3.paddr <= '0;
+            stage3.instr <= '0;
+
+            stage2.valid <= 0;
+            stage2.vaddr <= '0;
+            stage2.paddr <= '0;
+            
+            if (DBG)
+                $display("Flushed... setting pc to %x", exe_target_i);
+            
+            // stage1 is always set valid
+            pc <= exe_target_i;
+        end
 
     endtask
-    
+
+    assign paddr_offset = (stage3.paddr - imem_addr_o) * 8;
+
+    assign stage4_stall = ~dcode_ready_i;
+
     // stage 3 stalls if memory isn't valid
-    assign stage3_stall = ~imem_valid_i;
+    assign stage3_stall = (stage4_stall && stage4.valid) | ~imem_valid_i;
 
     // stage 2 stalls if stage 3 is stalled or if memory is not ready
     // to take a request or if tlb hasn't given us a paddr yet
     // TODO: how does tlb behave on a miss, will this work?
-    assign stage2_stall = stage3_stall | ~imem_ready_i | ~itlb_miss_i;
+    assign stage2_stall = (stage3_stall && stage3.valid) | ~imem_ready_i | itlb_miss_i;
 
-    assign stage1_stall = stage2_stall;
+    assign stage1_stall = (stage2_stall && stage2.valid);
 
     assign stage1.valid = 1;
     assign stage1.vaddr = pc;
@@ -138,9 +178,9 @@ module fetch (
     assign bp_valid_o = stage1.valid;
     assign bp_vaddr_o = pc;
     
-    assign dcode_valid_o = stage3.compl;
-    assign dcode_pc_o    = stage3.vaddr;
-    assign dcode_instr_o = stage3.instr;
+    assign dcode_valid_o = stage4.valid;
+    assign dcode_pc_o    = stage4.vaddr;
+    assign dcode_instr_o = stage4.instr;
 
     logic DBG;
     initial begin
@@ -150,39 +190,52 @@ module fetch (
         end
 
         pc = '0;
-
-        print_stages();
     end
 
     always_ff @(posedge clk) begin
+
+        print_stages();
         
         // stage 3 stuff
         // by now, its a safe assumption that we queried memory for our address,
         // so we should just set decode values, assuming memory came back
         if (stage3.valid && ~stage3_stall) begin
-            
+
             // this could be optimized to be so much better, and maybe i will
             // down the line. for now, it's unnecessary and we will do the
             // trivial version.
             if (imem_valid_i) begin
-                stage3.instr <= imem_rdata_i[31:0];
-                stage3.compl <= 1;
+                if (DBG)
+                    $display("Fetch State: Receiving data from memory");
+                //$display("offs %d, mem_addr %d, our addr %d itlb %d", paddr_offset, imem_addr_o, stage3.paddr, itlb_paddr_i);
+
+                assert(!(|paddr_offset[30:9]));
+                stage3.instr <= imem_rdata_i[paddr_offset[8:0] +: 32];
             end
 
         end
+        
+        if (imem_valid_o && imem_ready_i)
+            imem_valid_o <= 0;
 
-
-        imem_valid_o <= 0;
         if (stage2.valid && ~stage2_stall) begin
+            assert(itlb_hit_i);
 
             if (itlb_hit_i) begin
+                if (DBG)
+                    $display("Fetch State: Receiving paddr %x from iTLB", itlb_paddr_i);
+
                 stage2.paddr <= itlb_paddr_i;
+
+                // do we actually need to query?
+                if (!(imem_valid_i && (itlb_paddr_i > imem_addr_o) && ((itlb_paddr_i - imem_addr_o) * 8 <= 480))) begin
+                    imem_addr_o <= itlb_paddr_i;
+                    imem_valid_o <= 1;
+
+                end
+
             end
              
-            if (imem_ready_i) begin
-                imem_addr_o <= itlb_paddr_i;
-                imem_valid_o <= 1;
-            end // if this is false, should stall through assign statement
         end
 
         if (stage1.valid && ~stage1_stall) begin
--- frontend/tests/fetch_tb.sv
+++ frontend/tests/fetch_tb.sv
@@ -72,9 +72,161 @@ initial begin
     end
 end
 
-initial begin
+task reset_st;
+
+    flush = 1;
+    exe_target = '0;
+    dcode_ready = 1;
+    imem_rdata = '0;
+    imem_ready = 1;
+    imem_resp = 0;
+    itlb_hit = 0;
+    itlb_miss = 0;
+    itlb_paddr = '0;
+    bp_taken = 0;
+    bp_target = '0;
+
+    @(negedge clk_in);
+    flush = 0;
+
+endtask
+
+task test_reg_out;
+    dcode_ready = 1;
+    imem_ready = 1;
+
+    @(negedge clk_in);
+
+    assert(itlb_vaddr == 0);
+    itlb_hit = 1;
+    itlb_paddr = 10;
+    @(negedge clk_in);
+
+    assert(imem_req);
+    assert(imem_addr == 10);
+
+    imem_resp = 1;
+    imem_rdata[31:0] = 32'hfefefefe;
+
+    @(negedge clk_in);
     
-    $display("hello there\n");
+    assert(dcode_pc == 0);
+    assert(dcode_valid == 1);
+    assert(dcode_instr == imem_rdata[0 +: 32]);
+
+    reset_st();
+
+endtask
+
+task test_flush;
+    
+    @(negedge clk_in);
+
+    itlb_hit = 1;
+    itlb_paddr = 30'h323232f;
+    @(negedge clk_in);
+
+    assert(imem_req);
+    assert(imem_ready);
+    @(negedge clk_in);
+
+    flush = 1;
+    exe_target = 64'hfe3892143;
+    @(negedge clk_in);
+    flush = 0;
+    @(negedge clk_in);
+    @(negedge clk_in);
+    // did it flush? 
+    // it did :D
+
+    reset_st();
+endtask
+
+task test_branch;
+    
+    // don't care, run it thru
+    imem_resp = 1;
+
+    bp_taken = 1;
+    bp_target = 64'h3232;
+    @(negedge clk_in);
+    bp_taken = 0;
+    itlb_hit = 1;
+    @(negedge clk_in);
+    assert(itlb_vaddr == 64'h3232);
+    itlb_paddr = 30'h432;
+    assert(imem_req);
+    assert(imem_addr == 30'h0);
+    @(negedge clk_in);
+    itlb_paddr = 30'h832;
+    assert(imem_addr == 30'h432);
+    assert(imem_req);
+    assert(itlb_vaddr == 64'h3236);
+
+    @(negedge clk_in);
+    assert(imem_addr == 30'h832);
+
+    reset_st();
+
+endtask
+
+task test_offset_read;
+
+    imem_rdata[31:0] = 'h1;
+    imem_rdata[63:32] = 'h2;
+
+    @(negedge clk_in);
+    itlb_hit = 1;
+    itlb_paddr = 'h10;
+    @(negedge clk_in);
+    itlb_paddr = 'h14;
+    assert(imem_req);
+    assert(imem_addr == 'h10);
+    imem_resp = 1;
+    @(negedge clk_in);
+    assert(dcode_pc == 0);
+    assert(dcode_instr == 'h1);
+    @(negedge clk_in);
+    assert(dcode_pc == 4);
+    assert(dcode_instr == 'h2);
+    @(negedge clk_in);
+
+    reset_st();
+
+endtask
+
+task test_tlb_miss;
+
+    @(negedge clk_in);
+    itlb_miss = 1;
+
+    @(negedge clk_in);
+    assert(~imem_req);
+    @(negedge clk_in);
+    assert (~imem_req);
+    assert(itlb_vaddr == 0);
+    itlb_miss = 0;
+    itlb_hit = 1;
+    itlb_paddr = 'h20;
+    @(negedge clk_in);
+    assert(itlb_vaddr == 4);
+    assert(imem_req);
+    assert(imem_addr == 'h20);
+    @(negedge clk_in);
+    
+    reset_st();
+endtask
+
+initial begin
+    reset_st();
+    test_reg_out();
+    test_flush();
+    test_branch();
+    test_offset_read();
+    test_tlb_miss();
+    $display();
+    $display("passed all tests");
+    $finish();
 end
 
 endmodule
