2c930a570c5c6bfa13da31d79f1b7f2cd416ea84
Hugo Mireles <juicebox13521@gmail.com>
2026-04-24 19:13:07 -0500

    Alu progress, will link up tester

--- backend/units/alu.sv
+++ backend/units/alu.sv
@@ -3,7 +3,7 @@
 import types::*;
 
 module alu #(
-    parameter int unsigned RS_ENTRIES = 4,
+    parameter int unsigned DELAY = 1,
 	parameter int unsigned tagW = 6
 ) (
 	input logic clk,
@@ -19,25 +19,70 @@ module alu #(
     input logic [TAG_LEN-1:0] flag_tag,
     input logic should_output,
     input logic set_flags,
-    input fu_op_t op
+    input fu_op_t op,
 
-    output fu_result_t result;
+    output logic valid_out,
+    input logic ready_in,
+    output fu_result_t result
 );
 
-// Will finish quickly in morning I misunderstood how
-// we were linking registers
 
-// On start up set payload to not valid
-
-always_comb begin
-    
+initial begin 
+    valid_out = 1'b0;
+    ready_out = 1'b1;
+    result = 0;
+    count = 0;
 end
 
+// Keep the state over, cahnge the names or store in a struct
+logic working_valid;
+logic [63:0] working_arg1;
+logic [63:0] working_arg2;
+logic [TAG_LEN-1:0] working_tag;
+logic [TAG_LEN-1:0] working_flag_tag;
+logic working_should_output;
+logic working_set_flags;
+fu_op_t working_op;
+// Assign result in comb, only validate in ff
+
+always_comb begin
+    result.valid = working_valid && ready_out && count >= DELAY;
+    result.tag = working_tag;
+    result.value = working_arg1 + working_arg2;
+    // result.flags = TODO: Flag logic
+    result.flags_valid = working_set_flags;
+    //result.exception = ; TODO Exception logic
+    // result.exception_code = ;
+end
+
+logic[$clog2(DELAY):0] count;
 always_ff @(posedge clk) begin
     if(~rstN || flush) begin
-        issueReady <= 1'b1;
+        ready_out <= 1'b1;
     end else begin
+        // If valid conditions reached set valid_out to true
+        // Take in a value to compute, block the alu
+        if(valid_in && ready_out) begin
+            ready_out <= 1'b0;
+            working_valid <= 1'b1;
+            working_arg1 <= arg1;
+            working_arg2 <= arg2;
+            working_tag <= tag;
+            working_flag_tag <= flag_tag;
+            working_should_output <= should_output;
+            working_set_flags <= set_flags;
+            working_op <= op;
+            count <= 1'b1;
+        end else if(count >= DELAY && ready_in == 1'b1) begin
+            ready_out <= 1'b1;
+        end else begin
+            count <= count + 1;
+        end
 
+        // Output values to rob and invalidate the results
+        if(valid_out && ready_in) begin
+
+        end
     end
 end
 
@@ -88,14 +133,13 @@ rs_entry[RS_ENTRIES-1:0] entries;
 logic[$clog2(RS_ENTRIES):0] count;
 logic[$clog2(RS_ENTRIES)-1:0] selected;
 
+// On start up set payload to not valid
 initial begin 
     count = 0;
     issueReady = 1;
     valid_out = 0;
     selected = 0;
 end
-
-// On start up set payload to not valid
 
 logic[$clog2(RS_ENTRIES)-1:0] index_open;
 logic open_valid;
@@ -150,19 +194,23 @@ always_ff @(posedge clk) begin
         issueReady <= 1'b1;
     end else begin
         // Insert issue into new rs
-        if(issueValid && issueReady && count < RS_ENTRIES && open_valid) begin
+        // TODO ISSUE READY SHOULD BE SET IF THE COUNT IS CORRECT
+        if(issueValid && issueReady && open_valid) begin
             count <= count + 1;
             entries[index_open].valid <= 1'b1;
-            entries[index_open].waiting1 <= ~payload.src1_ready;
-            entries[index_open].waiting2 <= ~payload.src2_ready && payload.src2_valid;
-            entries[index_open].arg1 <= src1_value;
-            entries[index_open].arg2 <= payload.src2_valid ? payload.src2_value : payload.imm;
-            entries[index_open].reg1_tag <= payload.src1_tag;
-            entries[index_open].reg2_tag <= payload.src2_tag;
-            entries[index_open].result_tag <= payload.dest_tag;
+            entries[index_open].waiting1 <= ~payload_bus.src1_ready;
+            entries[index_open].waiting2 <= ~payload_bus.src2_ready && payload_bus.src2_valid;
+            entries[index_open].arg1 <= payload_bus.src1_value;
+            entries[index_open].arg2 <= payload_bus.src2_valid ? payload_bus.src2_value : payload_bus.imm;
+            entries[index_open].reg1_tag <= payload_bus.src1_tag;
+            entries[index_open].reg2_tag <= payload_bus.src2_tag;
+            entries[index_open].result_tag <= payload_bus.dest_tag;
             entries[index_open].set_flags <= cond;
             entries[index_open].op <= fu_op; 
+            // Add 1 to count
         end
+
+        issueReady <= count <= RS_ENTRIES ? 1'b1 : 1'b0;
 
         // Valid out should be based on if current output is good
 
@@ -172,16 +220,20 @@ always_ff @(posedge clk) begin
             // I think this should be overwritten if its still
             // valid next cycle
             valid_out <= 1'b0;
+            // Remove 1 from count
         end
 
         // Update rs' with cdb values
-        if(reg1_update_valid) begin
-            entries[index_reg1_update].arg1 <= cdb.value;
-            entries[index_reg1_update].waiting1 <= 1'b0;
-        end
-        if(reg2_update_valid) begin
-            entries[index_reg2_update].arg2 <= cdb.value;
-            entries[index_reg2_update].waiting2 <= 1'b0;
+        for(int i = 0; i < RS_ENTRIES; i++) begin
+            if(cdb_out.valid) begin
+                if(cdb_out.tag == entries[i].reg1_tag) begin
+                    entries[i].arg1 <= cdb.value;
+                    entries[i].waiting1 <= 1'b0;
+                end else if(cbd_out.tag == entries[i].reg2_tag) begin
+                    entries[i].arg2 <= cdb.value;
+                    entries[i].waiting2 <= 1'b0;
+                end
+            end 
         end
 
         // Output selected values
